dba74857b00ad0b52f635ea9e89c72aa8a0907d3
andres <adominguez@dvconsultores.com>
2014-12-27 16:13:34 -0430

    Ultimos commit, falta adaptación final a sqlserver

---
+++ openbizview/WebContent/scriptlib/postgres/datosini_openbizviewpg.sql
@@ -0,0 +1,4 @@
+insert into instancias values ('1','LOCAL','ADMIN',now(),'ADMIN',now());
+insert into bvt003 values ('ROL1','ROL UNICO','ADMIN',now(), 'ADMIN',now(),1);
+insert into bvt002 values ('ADMIN','ADMINISTRADOR','835d6dc88b708bc646d6db82c853ef4182fabbd4a8de59c213f2b5ab3ae7d9be','ROL1','ADMIN',now(),'ADMIN',now(),'mail@mail.com','1');
+commit;
