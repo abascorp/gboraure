42a17efd541deb3881e9af3acc9eea8d4e878b04
andres <adominguez@dvconsultores.com>
2016-05-02 18:18:07 -0400

    Versión 6 final

--- openbizview/WebContent/scriptlib/oracle/usr_openbizviewora.sql
+++
@@ -1,4 +0,0 @@
-create user openbizview
-identified by openbizview
-default tablespace bdopenbizview
-temporary tablespace temp;
--- openbizview/WebContent/scriptlib/postgres/bd_openbizviewpg.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE openbizview
-  WITH OWNER = bizview
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'es_VE.UTF-8'
-       LC_CTYPE = 'es_VE.UTF-8'
-       CONNECTION LIMIT = -1;
